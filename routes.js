8a86a17fa719857d0ec2c80d0750e003849f2d18
Wolf Oliver <owolf@anynines.com>
2018-04-12 08:53:20 +0200

    write all log messages to a file

--- routes.js
+++ routes.js
@@ -1,8 +1,18 @@
-var express = require('express');
-var router = express.Router();
-var pg = require('pg');
-var md5 = require('md5');
-var request = require('request');
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+const md5 = require('md5');
+const request = require('request');
+const winstonLogger = require('winston');
+const fileLoggertTransport = new winstonLogger.transports.File({ filename: 'users.log' });
+const consoleLoggerTransport = new winstonLogger.transports.Console();
+
+const logger = winstonLogger.createLogger({
+  format: winstonLogger.format.printf(info => `${info.level}: ${info.message}`),
+  transports: [
+    fileLoggertTransport
+  ]
+});
 
 var connection = pg.createConnection({
   host     : 'dockerhost',
@@ -13,10 +23,10 @@ var connection = pg.createConnection({
 
 connection.connect(function(err) {
   if (err) {
-    console.error('error connecting to db: ' + err.stack);
+    logger.error('error connecting to db: ' + err.stack);
     return;
   }
-  console.log('connected to db ');
+  logger.info('connected to db ');
 });
 
 router.get('/hello', function(req, res) {
@@ -26,7 +36,7 @@ router.get('/hello', function(req, res) {
 router.post('/create_table', function(req, res) {
   connection.query('CREATE TABLE IF NOT EXISTS users(id INT, firstname VARCHAR(32), lastname VARCHAR(32), age INT,  email VARCHAR(32), twittername VARCHAR(32), twittervalid BOOLEAN, avatar_url VARCHAR(100), PRIMARY KEY(id));', function(err, rows, fields) {
     if (err) {
-      console.log(err);
+      logger.info(err);
       res.status(500).json({success: false, msg: 'could not create table'});
     } else {
       res.status(200).json({success: true, msg: 'created table'});
@@ -47,9 +57,10 @@ router.post('/user', function(req, res) {
 router.get('/user', function(req, res){
   connection.query('SELECT * FROM users', function(err, result, fields){
     if(err){
-      console.log(err);
+      logger.info(err);
       res.status(500).json({success: false, msg: 'could not get users'});
     } else {
+      logger.info('list all users');
       res.status(200).json(result.rows);
     }
   });
@@ -58,9 +69,10 @@ router.get('/user', function(req, res){
 router.get('/user/:user_id', function(req, res){
   connection.query('SELECT * FROM users WHERE id = ' + req.params.user_id + ';', function(err, result, fields){
     if(err){
-      console.log(err);
+      logger.info(err);
       res.status(500).json({success: false, msg: 'could not get user details'});
     } else {
+      logger.info('show user details');
       res.status(200).json(result.rows[0]);
     }
   });
@@ -68,6 +80,7 @@ router.get('/user/:user_id', function(req, res){
 
 router.put('/user/:user_id', function(req, res) {
   twittercheck(req.body.twittername, function(exists){
+    logger.info('create new user');
     if(exists){
       edit_user(req, res, true);
     } else {
@@ -79,9 +92,10 @@ router.put('/user/:user_id', function(req, res) {
 router.delete('/user/:user_id', function(req, res){
   connection.query('DELETE FROM users WHERE id = ' + req.params.user_id + ';', function(err, rows, fields){
     if(err){
-      console.log(err);
+      logger.info(err);
       res.status(500).json({success: false, msg: 'could not get delete user'});
     } else {
+      logger.info('user deleted');
       res.status(200).json({success: true, msg: 'user deleted !'});
     }
   });
@@ -101,11 +115,11 @@ var twittercheck = function(name, callback){
 var add_user = function(req, res, twittervalid){
   var gravatarurl = 'https://www.gravatar.com/avatar/' + md5(req.body.email);
   var insert_statement = 'INSERT INTO users (id, firstname, lastname, age, email, twittername, twittervalid, avatar_url) values (coalesce((SELECT max(id)+1 FROM users), 0), \'' + req.body.firstname + '\', \'' + req.body.lastname + '\', ' + req.body.age + ', \'' + req.body.email + '\', \''+ req.body.twittername  + '\', ' + twittervalid + ', \'' + gravatarurl + '\');';
-  console.log(insert_statement);
+  logger.info(insert_statement);
 
   connection.query(insert_statement, function(err, rows, fields) {
     if (err) {
-      console.log(err);
+      logger.info(err);
       res.status(500).json({success: false, msg: 'could not add user'});
     } else {
       add_project(req.body.firstname + '_' + req.body.lastname + '_training');
@@ -117,11 +131,11 @@ var add_user = function(req, res, twittervalid){
 var edit_user = function(req, res, twittervalid){
   var gravatarurl = 'https://www.gravatar.com/avatar/' + md5(req.body.email);
   var update_statement = 'UPDATE users SET firstname = \'' + req.body.firstname + '\', lastname = \'' + req.body.lastname + '\', age = ' + req.body.age + ', email = \'' + req.body.email + '\', twittername = \''+ req.body.twittername  + '\', twittervalid = ' + twittervalid + ', avatar_url = \'' + gravatarurl + '\' where id = ' + req.params.user_id + ';';
-  console.log(update_statement);
+  logger.info(update_statement);
 
   connection.query(update_statement, function(err, rows, fields) {
     if (err) {
-      console.log(err);
+      logger.info(err);
       res.status(500).json({success: false, msg: 'could not add user'});
     } else {
       res.status(200).json({success: true, msg: 'user updated '});
@@ -138,10 +152,10 @@ var add_project = function(project_name){
 
   request(options, function(error, response, body){
     if (!error && response.statusCode == 200) {
-       console.log('project added');
+       logger.info('project added');
     }
     if (!error && response.statusCode == 404) {
-       console.log('could not add project');
+       logger.info('could not add project');
     }
   })
 }
